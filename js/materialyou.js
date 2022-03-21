b8ee4e896c9322b520000c6eb0cf89de900e5337
Attila Németh <attilanemeth@gmx.de>
2022-03-21 19:49:13 +0100

    Layout + Top App Bar

--- js/materialyou.js
+++ js/materialyou.js
@@ -7,26 +7,17 @@
 
 jQuery(document).ready(() => {
   jQuery(document).scroll(() => {
-    if(jQuery('body:not(.toolbar-fixed)').length > 0) {
-      if (jQuery(document).scrollTop() >= 39) {
-        jQuery('body').addClass('materialyou-top-app-bar-fixed');
-      }
-      else {
-        jQuery('body').removeClass('materialyou-top-app-bar-fixed');
-      }
-    }
     if (jQuery(document).scrollTop() > 0) {
-      jQuery('header.materialyou-top-app-bar').addClass('document-scrolled');
+      jQuery('body').addClass('document-scrolled');
     }
     else {
-      jQuery('header.materialyou-top-app-bar').removeClass('document-scrolled');
+      jQuery('body').removeClass('document-scrolled');
+    }
+    if (jQuery(document).scrollTop() > 39) {
+      jQuery('body').addClass('document-scrolled-beyond-bar');
+    }
+    else {
+      jQuery('body').removeClass('document-scrolled-beyond-bar');
     }
   });
-  var $topAppBar = jQuery('header.materialyou-top-app-bar');
-  if ($topAppBar.length > 0) {
-    jQuery('body').addClass('materialyou-has-top-app-bar');
-  }
-  else {
-    jQuery('body').addClass('materialyou-not-top-app-bar');
-  }
 });
